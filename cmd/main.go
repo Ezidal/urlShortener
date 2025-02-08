dc6d2f5918797a1f32ce072c8a8168699c3ebc47
Ezidal <sergeyefimov9593@gmail.com>
2025-02-08 17:15:05 +0300

    add storage sqlite with CRU methods

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"log"
+	"os"
 	"urlShortener/internal/config"
 	"urlShortener/internal/logger"
+	"urlShortener/internal/logger/er"
+	"urlShortener/internal/storage/sqlite"
 )
 
 func main() {
@@ -13,4 +16,19 @@ func main() {
 	}
 	log := logger.SetLogger(config.Environment)
 
+	log.Info("Service starting")
+
+	storage, err := sqlite.New(config.Storage_path)
+	if err != nil {
+		log.Error("failed to init db", er.Err(err))
+		os.Exit(1)
+	}
+	// err = storage.SaveUrl("https://google.com", "gogle")
+	storage.SaveUrl("popopoppo", "pop")
+	err = storage.DeleteUrl("pop")
+	if err != nil {
+		log.Error("failed to delete db", er.Err(err))
+	} else {
+		log.Info("успех")
+	}
 }
